c8edf4670e364e53eceae02cc7ae4f3bacaccb5e
snowmanunderwater <snowmanunderwater@gmail.com>
2018-09-06 21:10:18 +0300

    Delete button

--- blog/utils.py
+++ blog/utils.py
@@ -10,7 +10,15 @@ class ObjectsDetailMixin:
 
     def get(self, request, slug):
         obj = get_object_or_404(self.model, slug__iexact=slug)
-        return render(request, self.template, context={self.model.__name__.lower(): obj, 'admin_object': obj, 'detail': True})
+        return render(
+            request,
+            self.template,
+            context={
+                self.model.__name__.lower(): obj,
+                "admin_object": obj,
+                "detail": True,
+            },
+        )
 
 
 class ObjectCreateMixin:
@@ -19,7 +27,7 @@ class ObjectCreateMixin:
 
     def get(self, request):
         form = self.model_form
-        return render(request, self.template, context={'form': form})
+        return render(request, self.template, context={"form": form})
 
     def post(self, request):
         bound_form = self.model_form(request.POST)
@@ -28,7 +36,7 @@ class ObjectCreateMixin:
             new_obj = bound_form.save()
             return redirect(new_obj)
 
-        return render(request, self.template, context={'form': bound_form})
+        return render(request, self.template, context={"form": bound_form})
 
 
 class ObjectUpdateMixin:
@@ -39,8 +47,11 @@ class ObjectUpdateMixin:
     def get(self, request, slug):
         obj = self.model.objects.get(slug__iexact=slug)
         bound_form = self.model_form(instance=obj)
-        return render(request, self.template, context={
-            'form': bound_form, self.model.__name__.lower(): obj})
+        return render(
+            request,
+            self.template,
+            context={"form": bound_form, self.model.__name__.lower(): obj},
+        )
 
     def post(self, request, slug):
         obj = self.model.objects.get(slug__iexact=slug)
@@ -49,18 +60,23 @@ class ObjectUpdateMixin:
         if bound_form.is_valid():
             new_obj = bound_form.save()
             return redirect(new_obj)
-        return render(request, self.template, context={
-            'form': bound_form, self.model.__name__.lower(): obj})
+        return render(
+            request,
+            self.template,
+            context={"form": bound_form, self.model.__name__.lower(): obj},
+        )
 
 
-class ObjectDeleteMixin():
+class ObjectDeleteMixin:
     model = None
     template = None
     redirect_url = None
 
     def get(self, request, slug):
         obj = self.model.objects.get(slug__iexact=slug)
-        return render(request, self.template, context={self.model.__name__.lower(): obj})
+        return render(
+            request, self.template, context={self.model.__name__.lower(): obj}
+        )
 
     def post(self, request, slug):
         obj = self.model.objects.get(slug__iexact=slug)
