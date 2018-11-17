95e539395bfd5aae27b770613b6bb1f75657f1e6
snowmanunderwater <snowmanunderwater@gmail.com>
2018-11-17 23:37:04 +0300

    fix

--- blog/views.py
+++ blog/views.py
@@ -1,8 +1,7 @@
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.core.paginator import Paginator
 from django.db.models import Q
-from django.shortcuts import get_object_or_404, redirect, render
-from django.urls import reverse
+from django.shortcuts import render
 from django.views.generic import View
 
 from .forms import PostForm, TagForm
